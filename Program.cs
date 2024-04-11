183bf2ea38bf53359e14052d7b489edb463bd417
XEonAX <sv.xeon@gmail.com>
2024-04-11 17:39:54 +0530

    Add Cors support, for web-based Server Mgmt UI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +16,21 @@ builder.Services.AddHostedService(provider => provider.GetService<Communications
 builder.Services.AddHttpClient<Communications>();
 builder.Services.AddSingleton<IMultiplayerServer, MultiplayerServer>();
 builder.Services.AddHostedService<ServerHost>();
-
-builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(
+            policy =>
+            {
+                policy.WithOrigins("http://localhost:3000",
+                                    "https://studios.aeonax.com")
+                        .WithMethods("GET", "POST")
+                        .AllowAnyHeader();
+            });
+    });
+builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;//Use original property names
+    }); ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,9 +46,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors();
 
 app.MapControllers();
-
 app.Run();
